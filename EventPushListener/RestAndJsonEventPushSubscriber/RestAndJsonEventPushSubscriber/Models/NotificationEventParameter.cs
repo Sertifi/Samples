2f98b3212957e8ffe5c40b01aec949b7e8239b57
Ross <rhall@sertifi.com>
2021-03-30 16:55:14 -0500

    ITG-150 removed unnecessary clutter.

--- EventPushListener/RestAndJsonEventPushSubscriber/RestAndJsonEventPushSubscriber/Models/NotificationEventParameter.cs
+++ EventPushListener/RestAndJsonEventPushSubscriber/RestAndJsonEventPushSubscriber/Models/NotificationEventParameter.cs
@@ -1,68 +1,21 @@
 ﻿namespace RestAndJsonEventPushSubscriber.Models
 {
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name = "NotificationEventParameter", Namespace = "http://apps.sertifi.net/services/subscribers")]
-    [System.SerializableAttribute()]
-    public partial class NotificationEventParameter : object, System.ComponentModel.INotifyPropertyChanged
+    /// <summary>
+    /// Parameters specific to the event that triggered the notification.
+    /// </summary>
+    public class NotificationEventParameter
     {
         /// <summary>
         /// The name of the parameter field.
         /// </summary>
         /// <example>PaymentName</example>
-        [System.Runtime.Serialization.OptionalFieldAttribute()]
-        private string NameField;
+        public string NameField { get; set; }
 
         /// <summary>
         /// The value of the parameter.
         /// </summary>
         /// <example>Down payment</example>
-        [System.Runtime.Serialization.OptionalFieldAttribute()]
-        private string ValueField;
+        public string ValueField { get; set; }
 
-        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false)]
-        public string Name
-        {
-            get
-            {
-                return this.NameField;
-            }
-            set
-            {
-                if ((object.ReferenceEquals(this.NameField, value) != true))
-                {
-                    this.NameField = value;
-                    this.RaisePropertyChanged("Name");
-                }
-            }
-        }
-
-        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false)]
-        public string Value
-        {
-            get
-            {
-                return this.ValueField;
-            }
-            set
-            {
-                if ((object.ReferenceEquals(this.ValueField, value) != true))
-                {
-                    this.ValueField = value;
-                    this.RaisePropertyChanged("Value");
-                }
-            }
-        }
-
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-
-        protected void RaisePropertyChanged(string propertyName)
-        {
-            System.ComponentModel.PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
-            if ((propertyChanged != null))
-            {
-                propertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
-            }
-        }
     }
 }
